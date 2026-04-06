eb29320eb5ac48c90af587c04479977b1bfbe5e2
nahuelgonzalez0 <ng0767185@gmail.com>
2026-04-06 01:55:54 -0300

    Componente titulo terminado TP-9

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './home.module.css';
+import styles from './home.module.css';
+import Titulo from '../../components/titulo/titulo'
 
 const home = () => {
     return (
-        <div className="container">
-            <h1>Gestor de peliculas</h1>
+        <div className={styles.container}>
+            <Titulo texto="Gestor de peliculas" /> 
             <p>Pagina principal</p>
         </div>
     )
