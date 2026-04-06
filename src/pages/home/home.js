2631f87eba4974ade7314c9736f87e4e956b213b
nahuelgonzalez0 <ng0767185@gmail.com>
2026-04-06 01:09:40 -0300

    Estructura de carpetas y componente Home TP-7

---
+++ src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.module.css';
+
+const home = () => {
+    return (
+        <div className="container">
+            <h1>Gestor de peliculas</h1>
+            <p>Pagina principal</p>
+        </div>
+    )
+}
+
+export default home
