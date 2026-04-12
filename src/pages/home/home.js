6b03909646151976552ac227210e118b5fe3f622
nasusxd <js8918657@gmail.com>
2026-04-11 21:12:15 -0300

    agregue estado en movicard y cambie problemas con la keys

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './home.module.css';
 import Titulo from '../../components/titulo/titulo'
 import Boton from '../../components/boton/boton'
@@ -9,82 +9,102 @@ import Select from '../../components/select/select';
 import Input from '../../components/input/input';
 
 const Peliculas = [
-  { imagen: "", titulo: "Inception", genero: "ciencia ficcion", anio: 2010 },
-  { imagen: "", titulo: "Titanic", genero: "romance", anio: 1997 },
-  { imagen: "", titulo: "The Dark Knight", genero: "accion", anio: 2008 },
-  { imagen: "", titulo: "Interstellar", genero: "ciencia ficcion", anio: 2014 },
-  { imagen: "", titulo: "Superbad", genero: "comedia", anio: 2007 },
-  { imagen: "", titulo: "The Hangover", genero: "comedia", anio: 2009 }
+  { id: 1, imagen: "", titulo: "Inception", genero: "ciencia ficcion", anio: 2010, estado: true },
+  { id: 2, imagen: "", titulo: "Titanic", genero: "romance", anio: 1997, estado: false },
+  { id: 3, imagen: "", titulo: "The Dark Knight", genero: "accion", anio: 2008, estado: true },
+  { id: 4, imagen: "", titulo: "Interstellar", genero: "ciencia ficcion", anio: 2014, estado: true },
+  { id: 5, imagen: "", titulo: "Superbad", genero: "comedia", anio: 2007, estado: false },
+  { id: 6, imagen: "", titulo: "The Hangover", genero: "comedia", anio: 2009, estado: true }
 ];
+
 
 const Home = () => {
   const [peliculas, setPeliculas] = useState(() => {
     const guardadas = localStorage.getItem("peliculas");
     return guardadas ? JSON.parse(guardadas) : Peliculas;
-});
+  });
 
-const [peliculaSeleccionada, setPeliculaSeleccionada] = useState(null);
+  const [peliculaSeleccionada, setPeliculaSeleccionada] = useState(null);
 
-const [busqueda, setBusqueda ] = useState("")
+  const [busqueda, setBusqueda] = useState("")
 
-useEffect(() => {
-  localStorage.setItem("peliculas", JSON.stringify(peliculas));
-},[peliculas]);
+  useEffect(() => {
+    localStorage.setItem("peliculas", JSON.stringify(peliculas));
+  }, [peliculas]);
 
-const editarPelicula = (pelicula) => {
-  setPeliculaSeleccionada(pelicula);
+  const editarPelicula = (pelicula) => {
+    setPeliculaSeleccionada(pelicula);
+  };
+
+const editarEstado = (id) => {
+  const actPeli = peliculas.map((i) =>
+    i.id === id ? { ...i, estado: !i.estado } : i
+  );
+  setPeliculas(actPeli);
 };
 
 const onClickEditar = (peliculaEditada) => {
   const nuevaPelicula = peliculas.map((p) =>
-    p.titulo === peliculaSeleccionada.titulo ? peliculaEditada : p);
+    p.id === peliculaSeleccionada.id ? peliculaEditada : p);
   setPeliculas(nuevaPelicula);
   setPeliculaSeleccionada(null);
 };
 
 const peliculasFiltradas = peliculas.filter((peli) =>
-peli.titulo.toLowerCase().includes(busqueda.toLowerCase())
+  peli.titulo.toLowerCase().includes(busqueda.toLowerCase())
 );
 
 const generos = [...new Set(peliculas.map(p => p.genero))];
 
-  return (
-      <div className={styles.container}>
-          <Titulo texto="Gestor de peliculas" /> 
-          <p>Pagina principal</p>
+return (
+  <div className={styles.container}>
+    <Titulo texto="Gestor de peliculas" />
+    <p>Pagina principal</p>
 
-          <Input label="Buscador" type="text" placeholder="Ingrese el titulo de la pelicula" value={busqueda} onChange={(e) => setBusqueda(e.target.value)} />
-          
-          <Boton texto="Agregar Pelicula" funcion=""/>
-          <Contador Peliculas={peliculas}/>
-          <br />
-          <Select opciones={generos.map(g => ({ value: g, label: g }))}/>
-          <br /><br /><br />
+    <Input label="Buscador" type="text" placeholder="Ingrese el titulo de la pelicula" value={busqueda} onChange={(e) => setBusqueda(e.target.value)} />
 
-          <div className={styles.containerMovieCard}>
-              {peliculasFiltradas.map((pelicula, index) => (
-                <MovieCard 
-                key={index}
-                imagen={pelicula.imagen}
-                titulo={pelicula.titulo}
-                genero={pelicula.genero}
-                anio={pelicula.anio}
-                clickEditar={editarPelicula}
-                />
-                ))}
-          </div>
+    <Boton texto="Agregar Pelicula" funcion="" />
+    <Contador Peliculas={peliculas} />
+    <br />
+    <Select opciones={generos.map(g => ({ value: g, label: g }))} />
+    <br /><br /><br />
 
-          {peliculaSeleccionada && ( 
-            <div className={styles.containerForm}>
-              <Formulario
-                titulo="Editar Película"
-                pelicula={peliculaSeleccionada}
-                onClick={onClickEditar}
-              />
-            </div>
-          )}
+    <div className={styles.containerMovieCard}>
+      {peliculasFiltradas.map((pelicula, id) => (
+        <MovieCard
+          key={pelicula.id}
+          id={pelicula.id}
+          imagen={pelicula.imagen}
+          titulo={pelicula.titulo}
+          genero={pelicula.genero}
+          anio={pelicula.anio}
+          estado={pelicula.estado}
+          clickEditar={editarPelicula}
+          cambiarEstado={editarEstado}
+        />
+      ))}
     </div>
-  )
+
+    {peliculaSeleccionada && (
+      <div className={styles.containerForm}>
+        <Formulario
+          titulo="Editar Película"
+          pelicula={peliculaSeleccionada}
+          onClick={onClickEditar}
+        />
+      </div>
+    )}
+    <div>
+
+
+
+    </div>
+  </div>
+
+
+
+
+)
 }
 
 export default Home
